98554b6a822bc10493d04c7aae83c215f4088ad1
b.sass@innovativethinking.co.za <b.sass@innovativethinking.co.za>
2022-06-18 20:48:35 +0200

    prod1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MyGalleryComponent } from './my-gallery/my-gallery.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyGalleryComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    IvyCarouselModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/my-gallery/my-gallery.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-my-gallery',
+  templateUrl: './my-gallery.component.html',
+  styleUrls: ['./my-gallery.component.scss'],
+})
+export class MyGalleryComponent implements OnInit {
+  data: any[] = [];
+  images: any[] = [];
+  topics: any;
+  topicsBool = true;
+  loaderBool = false;
+  topic = '';
+  imageTotal = 3;
+  pageIndex = 0;
+  url = 'https://api.unsplash.com/topics/';
+  apiId = 'oTsf5M6-RS2bjI6bMnzy-fasHMqpWq1PON1oy2Tj-5g';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getTopics().then(() => {});
+  }
+
+  async getTopics() {
+    this.http
+      .get(`${this.url}?client_id=${this.apiId}`)
+      .subscribe((topics: any) => {
+        this.topics = topics;
+        this.getImages(topics[0]);
+      });
+  }
+
+  getImages(obj: any) {
+    this.loaderBool = true;
+    this.topicsBool = false;
+    this.images = [];
+    this.http
+      .get(`${this.url}${obj.id}/photos/?client_id=${this.apiId}`)
+      .subscribe((data: any) => {
+        this.loaderBool = false;
+        this.topic = obj.title;
+        data.forEach((elm: any) => {
+          this.images.push({ path: elm.urls['full'] });
+        });
+      });
+  }
+
+  onMove() {
+    this.topicsBool = false;
+  }
+
+  onMenu() {
+    this.topicsBool = true;
+  }
+}
